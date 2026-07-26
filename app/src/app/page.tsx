6d139a5f831b0f237235bc78c72fdce5d6875c15
asmitbiswas <asmitbiswas1308@gmail.com>
2026-07-26 20:45:33 +0530

    feat: Revision

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-    <p>
-      
+    <p className="text-2xl text-center">
+      hello
     </p>
    </div>
   );
